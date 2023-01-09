ed2ae454387610160c726a1aadc3aafada426a65
EgorkaEK <76541105+EgorkaEK@users.noreply.github.com>
2023-01-09 21:28:33 +0600

    Add files via upload

---
+++ ProjectWork2/ProjectWork2/ProjectWork2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+float calcVoltage(float resistance, float current)
+{
+    return resistance * current;
+}
+
+int main()
+{
+    float resistance, current;
+
+    setlocale(LC_ALL, "");
+
+    std::cout << "Введите сопротивление: ";
+    std::cin >> resistance;
+
+    std::cout << "Введите ток: ";
+    std::cin >> current;
+
+    float voltage = calcVoltage(resistance, current);
+
+    std::cout << "Напряжение: " << voltage << " V" << std::endl;
+
+    return 0;
+}
